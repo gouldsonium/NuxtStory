c6cf77eeca6942411698027d12f9e3a6996284aa
Stefan Gouldson <stefangouldson@gmail.com>
2023-07-23 14:59:57 +0100

    fixed hero section

--- storyblok/Hero.vue
+++ storyblok/Hero.vue
@@ -1,14 +1,12 @@
 <script setup>
-  import { defineProps, computed } from 'vue';
-
   const props = defineProps({ blok: Object });
 
-  const hasBackgroundImage = !!props.blok.background_image?.filename;
-  const hasBackgroundVideo = !!props.blok.background_video?.filename;
-  const backgroundColor = props.blok.background_color?.color;
+  // For the background style object
+  const hasBackgroundImage = computed(() => {return !!props.blok.background_image?.filename});  
+  const hasBackgroundColor = computed(() => {return !!props.blok.background_color?.color});
 
   const backgroundStyle = computed(() => {
-    if (hasBackgroundImage) {
+    if (hasBackgroundImage.value) {
       return {
         backgroundImage: `linear-gradient(to right, #22283180, #22283180), url(${props.blok.background_image.filename})`,
         backgroundPosition: 'center',
@@ -17,8 +15,8 @@
       };
     }
 
-    if(!!backgroundColor){
-      return {backgroundColor: backgroundColor}
+    if(hasBackgroundColor.value){
+      return {backgroundColor: props.blok.background_color?.color}
     }
 
     return null;
@@ -29,12 +27,12 @@
   <section
     v-editable="blok"
     class="min-h-[700px] relative flex items-center justify-center overflow-hidden"
-    :class="{ 'pt-32 lg:pt-0': !!blok.image.filename}"
+    :class="{ 'pt-32 lg:py-12': !!blok.image?.filename}"
     :style="backgroundStyle"
   >
     <!-- If background video exists, add the <video> element -->
     <video
-      v-if="hasBackgroundVideo"
+      v-if="!!blok.background_video?.filename"
       autoplay
       muted
       loop
@@ -42,17 +40,17 @@
       preload="auto"
       class="absolute top-0 left-0 w-full h-full object-cover z-0"
     >
-      <source :src="props.blok.background_video.filename" type="video/mp4" />
+      <source :src="blok.background_video?.filename" type="video/mp4" />
       <!-- Add more <source> elements for other video formats if needed -->
     </video>
 
     <div class="flex flex-col lg:flex-row justify-center items-center z-10 w-full text-center py-6 px-5 min-h-full">
-      <div :class="hasBackgroundImage || hasBackgroundVideo ? 'text-white' : 'text-gray-900'">
-        <h1 class="text-6xl font-bold mb-3" :style="{color: blok.title_color.color}">{{ blok.title }}</h1>
-        <h2 class="text-4xl font-light" :style="{color: blok.text_color.color}">{{ blok.text }}</h2>
+      <div class="text-gray-900">
+        <h1 class="text-6xl font-bold mb-3" :style="{color: blok.title_color?.color}">{{ blok.title }}</h1>
+        <h2 class="text-4xl font-light" :style="{color: blok.text_color?.color}">{{ blok.text }}</h2>
       </div>
-      <div class="w-full lg:w-1/2 p-5" v-if="!!blok.image.filename">
-        <img :src="blok.image.filename" alt="hero-img">
+      <div class="w-full lg:w-1/2 p-5" v-if="!!blok.image?.filename">
+        <img :src="blok.image?.filename" alt="hero-img">
       </div>
     </div>
   </section>
