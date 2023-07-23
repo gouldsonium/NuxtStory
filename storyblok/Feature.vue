23959fd3656194c15de22bbb080dde64bf60ddad
Stefan Gouldson <stefangouldson@gmail.com>
2023-07-23 23:39:42 +0100

    slight update

--- storyblok/Feature.vue
+++ storyblok/Feature.vue
@@ -4,7 +4,7 @@
     class="w-full flex flex-col justify-center items-center p-12 bg-primary"
     :style="{backgroundColor: blok.background_color?.color}"
   >
-    <div :class="blok.constrained ? 'md:w-1/2 w-full' : 'w-full'" class="px-3 py-6">
+    <div :class="blok.constrained ? 'md:w-1/2 w-full' : 'w-full'" class="md:px-3 py-6">
       <h2 v-if="!!blok.name" class="text-4xl text-secondary font-bold" :style="{color: blok.title_color?.color}">
         {{ blok.name }}
       </h2>
@@ -18,7 +18,7 @@
       v-if="!!blok.image?.filename"
       :src="blok.image.filename"
       :alt="blok.image.alt"
-      class="max-h-[400px] object-cover w-full px-3"
+      class="max-h-[400px] object-cover w-full md:px-3"
       :class="imageClass"
     />
   </div>
@@ -29,14 +29,14 @@
   const resolvedRichText = computed(() => renderRichText(props.blok.content))
 
   const layoutClass = computed(() => {
-    return [
-      props.blok.layout == 'left' ? 'md:flex-row' : '',
-      props.blok.layout == 'right' ? 'md:flex-row-reverse' : '',
-      props.blok.layout == 'top' ? 'md:flex-col' : '',
-      props.blok.layout == 'bottom' ? 'md:flex-col-reverse' : '',
-      props.blok.text_center == true ? 'text-center' : ''
-    ]
-  });
+  const { layout, text_center } = props.blok;
+  return [
+    layout === 'left' ? 'md:flex-row' : layout === 'right' ? 'md:flex-row-reverse' 
+    : layout === 'top' ? 'md:flex-col' : layout === 'bottom' ? 'md:flex-col-reverse' : '',
+    text_center ? 'text-center' : ''
+  ];
+});
+
 
   const imageClass = computed(() => {
     return [
--- storyblok/PopularArticles.vue
+++ storyblok/PopularArticles.vue
@@ -1,15 +1,15 @@
 <template>
-    <div class="py-24">
-      <h2 class="text-4xl text-secondary font-bold text-center mb-12">{{ blok.headline }}</h2>
-      <div class="container mx-auto grid md:grid-cols-3 gap-12 my-12 place-items-start">
-        <ArticleCard
-          v-for="article in blok.articles"
-          :key="article.uuid"
-          :article="article.content"
-          :slug="article.full_slug"
-        />
-      </div>
+  <div class="py-24">
+    <h2 class="text-4xl text-secondary font-bold text-center mb-12">{{ blok.headline }}</h2>
+    <div class="container mx-auto grid md:grid-cols-3 gap-12 my-12 place-items-start">
+      <ArticleCard
+        v-for="article in blok.articles"
+        :key="article.uuid"
+        :article="article.content"
+        :slug="article.full_slug"
+      />
     </div>
+  </div>
 </template>
    
 <script setup>
