4fd02b230c7c038df72931146ecd2f22f735e579
Stefan Gouldson <stefangouldson@gmail.com>
2023-07-23 17:52:08 +0100

    added base btn

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,5 +1,5 @@
 <script setup>
-  import { ref, onMounted, onUnmounted } from 'vue';
+  // import { ref, onMounted, onUnmounted } from 'vue';
   import { Dialog, DialogPanel } from '@headlessui/vue';
   import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline';
 
@@ -8,14 +8,38 @@
     version: 'draft',
     resolve_links: 'url',
   });
+
+  // Fetch Navbar Logo
+  const logo = data.story.content.logo?.filename || null;
   
   // Define header menu as a ref
   const headerMenu = ref(null);
-  headerMenu.value = data.story.content.header_menu.filter(item => !item.featured);
-  // Get call to actions
-  const CTA = data.story.content.header_menu.find(item => !!item.featured);
-  // Fetch Navbar Logo
-  const logo = data.story.content.logo?.filename || null;
+  headerMenu.value = data.story.content.header_menu;
+
+  // Handle call to action button
+  const callToAction = data.story.content?.cta[0];
+  const buttonBackgroundColor = ref(callToAction.btn_color?.color);
+  const buttonTextColor = ref(callToAction.text_color?.color);
+
+  const btnClasses = [
+    callToAction.style,
+    callToAction.hover == 'fade' ? 'hover:opacity-50' : ''
+  ]
+
+  const changeBackgroundColor = () => {
+    if(callToAction.hover == 'flip'){
+      buttonBackgroundColor.value = callToAction.text_color?.color;
+      buttonTextColor.value = callToAction.btn_color?.color;
+    } else if(callToAction.hover == 'transparent'){
+      buttonBackgroundColor.value = '#ffffff00'
+    }
+  };
+
+  const changeBackgroundBack = () => {
+    buttonBackgroundColor.value = callToAction.btn_color?.color;
+    buttonTextColor.value = callToAction.text_color?.color;
+  }
+
   // handle headers colors and scroll behaviour
   const showBackground = !data.story.content.background_on_scroll;
   const header_color = data.story.content?.header_color.color;
@@ -55,7 +79,7 @@
         </NuxtLink>
       </div>
       <div class="flex lg:hidden">
-        <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = true">
+        <button type="button" :style="{color: header_text_color}" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = true">
           <span class="sr-only">Open main menu</span>
           <Bars3Icon class="h-6 w-6" aria-hidden="true" />
         </button>
@@ -67,8 +91,15 @@
         >
           {{ blok.text }}
         </AppLink>
-        <AppLink :to="CTA.url" class="text-sm font-semibold leading-6 transition duration-300 btn">
-          {{ CTA.text }}
+        <AppLink v-if="callToAction.url && callToAction.text" :to="callToAction?.url" 
+          :class="btnClasses" class="text-sm font-semibold leading-6 btn" 
+          :style="{
+            backgroundColor: buttonBackgroundColor, color: buttonTextColor, 
+            border:`${callToAction.btn_color?.color} solid 2px`
+          }"
+          @mouseover="changeBackgroundColor('#e0e0e0')" @mouseout="changeBackgroundBack"
+        >
+          {{ callToAction?.text }}
         </AppLink>
       </div>
     </nav>
@@ -83,7 +114,7 @@
             <span class="sr-only">Logo</span>
             <img class="h-8 sm:h-16 w-auto dark-brighten" :src="logo" alt="Logo" />
           </NuxtLink>
-          <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
+          <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false" :style="{color: header_text_color}">
             <span class="sr-only">Close menu</span>
             <XMarkIcon class="h-6 w-6" aria-hidden="true" />
           </button>
@@ -97,8 +128,12 @@
                 >
                 {{ link.text }}
               </AppLink>
-              <AppLink :to="CTA.url" class="inline-block -mx-3 rounded-lg text-base font-semibold leading-7 btn">
-                {{ CTA.text }}
+              <AppLink
+                v-if="callToAction.url && callToAction.text" :class="btnClasses"
+                :to="callToAction.url" class="inline-block -mx-0 rounded-lg text-base font-semibold leading-7 btn" 
+                :style="{backgroundColor: buttonBackgroundColor, color: buttonTextColor}"
+              >
+                {{ callToAction.text }}
               </AppLink>
             </div>
           </div>
