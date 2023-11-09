6b0905db66529917440b2540c8e2adb0bf0c2fe3
Stefan Gouldson <stefangouldson@gmail.com>
2023-11-09 00:41:53 +0000

    added sitemap + fixed blog og image

--- components/islands/PageOgImage.vue
+++ components/islands/PageOgImage.vue
@@ -7,23 +7,23 @@
 
 <template>
   <div
-    style="position: relative; width: 100%; height: 100%; display: flex; flex-direction: column; background: white; color: black; align-items: center; justify-content: center;">
-    <div style="position: absolute; top: 4rem; left: 4rem;">
-      <img width="125" height="87" src="https://a.storyblok.com/f/247081/513x356/210de78197/rosbotham-logo.png" 
-      style="" alt="Rosbotham Logo" />
-    </div>
+    style="position: relative; width: 100%; height: 100%; display: flex; flex-direction: column; background: white; color: black; align-items: flex-start; justify-content: flex-start; padding-left: 5%; background-image: url('https://rosbothamfinance.com/blogBackground.png');">
     <h1
-      style="max-width: 80%; font-size: 4.375rem; text-align: center; color: #00ccff; margin-bottom: 25px;">
-      {{ title }}
-    </h1>
-    <p style="font-size: 2rem; max-width: 80%; color: #10336e;">{{ description }}</p>
-    <div style="display: flex; align-items: center; position: absolute; bottom: 4rem; right: 4rem;">
-      <img src="https://i.imgur.com/sFdtpA6.png"
+    style="max-width: 50%; font-size: 3rem; text-align: left; color: #00ccff; margin-bottom: 25px; margin-top: 55px;">
+    {{ title }}
+  </h1>
+  <p style="font-size: 2rem; max-width: 70%; color: #10336e;">{{ description }}</p>
+  <div style="position: absolute; bottom: 4rem; left: 4rem;">
+    <img width="125" height="87" src="https://a.storyblok.com/f/247081/513x356/210de78197/rosbotham-logo.png" 
+    style="" alt="Rosbotham Logo" />
+  </div>
+  <!-- <div style="display: flex; justify-content: center; align-items: center; position: absolute; bottom: 4rem; right: 4rem;">
+    <img src="https://i.imgur.com/sFdtpA6.png"
         style="border-radius: 50%; height: 6rem; width: 6rem; margin-right: 1rem; object-fit: cover;" />
       <div>
         <p style="font-size: 2rem; font-weight: 600; color: #10336e;">David Rosbotham</p>
         <p style="font-size: 1.5rem;">Founder</p>
       </div>
-    </div>
+    </div> -->
   </div>
 </template>
