4f7d41b1ac6fdf4d9b990cf307e05746a973cdff
Stefan Gouldson <stefangouldson@gmail.com>
2024-03-03 16:20:26 +0000

    hero updated

--- storyblok/Hero.vue
+++ storyblok/Hero.vue
@@ -1,13 +1,6 @@
 <script setup>
-  import { ref, onMounted, computed } from 'vue';
+  import { ref, onMounted } from 'vue';
   const props = defineProps({ blok: Object });
-
-  const backgroundImageLoaded = ref(false);
-
-  // Function to handle the image load event
-  const handleImageLoad = () => {
-    backgroundImageLoaded.value = true;
-  };
 
   // Run this code after the component is mounted
   onMounted(() => {
@@ -16,34 +9,38 @@
     image.src = props.blok.background_image?.filename;
   });
 
-  // Define the computed property for the background image style
-  const imageBoxStyle = computed(() => {
-    return {
-      backgroundImage: backgroundImageLoaded.value
-        ? `linear-gradient(to right, #22283180, #22283180), url(${props.blok.background_image?.filename})`
-        : 'none',
-      visibility: backgroundImageLoaded.value ? 'visible' : 'hidden',
-      backgroundColor: backgroundImageLoaded.value ? '#ffffff' : '#10336e'
-    };
-  });
+  // Function to handle the image load event
+  const handleImageLoad = () => {
+    // Trigger reactivity when the image is loaded
+    imageLoaded.value = true;
+  };
+
+  // Define the reactive property for image loaded state
+  const imageLoaded = ref(false);
 </script>
 
 <template>
-  <section class="out-box" v-editable="blok" :style="{backgroundColor: blok.background_color?.color}">
-    <div class="shape-box">
-      <div class="image-box" :style="imageBoxStyle">
-        <div class="grid grid-cols-1 lg:grid-cols-3 z-10 w-full py-6 sm:px-5 min-h-full container py-48">
-          <div class="text-white flex flex-col justify-center px-8 sm:px-24 lg:col-span-2" :class="blok.text_position">
-            <h1 class="mb-3">{{ blok?.title || 'Page Title' }}</h1>
-            <p class="font-light">{{ blok?.text }}</p>
-            <div class="mt-10 flex flex-wrap" v-if="blok?.buttons">
-              <StoryblokComponent
-                class="mr-3 mb-3 w-full sm:w-auto"
-                v-for="blok in blok.buttons"
-                :key="blok._uid"
-                :blok="blok"
-              />
-            </div>
+  <section v-editable="blok" :style="{backgroundColor: blok.background_color?.color}">
+    <div class="image-box" :style="{minHeight: `${blok.height}px`}">
+      <img 
+        v-if="blok.background_image" 
+        :src="blok.background_image?.filename" 
+        alt="Background Image" 
+        class="img-background" 
+        :class="blok?.background_image_darken"
+        :style="{maxHeight: `${blok?.height}px`}"
+      >
+      <div class="grid grid-cols-1 lg:grid-cols-3 z-10 w-full py-6 sm:px-5 min-h-full container py-48">
+        <div class="text-white flex flex-col justify-center px-8 sm:px-24 lg:col-span-2" :class="blok.text_position">
+          <h1 class="mb-3 z-30" :style="{color: blok.title_color?.color}">{{ blok?.title || 'Page Title' }}</h1>
+          <p class="font-light z-30" :style="{color: blok.text_color?.color}">{{ blok?.text }}</p>
+          <div class="mt-10 flex flex-wrap z-30" v-if="blok?.buttons">
+            <StoryblokComponent
+              class="mr-3 mb-3 w-full sm:w-auto"
+              v-for="blok in blok.buttons"
+              :key="blok._uid"
+              :blok="blok"
+            />
           </div>
         </div>
       </div>
@@ -53,13 +50,16 @@
 
 <style scoped>
 .image-box{
-  min-height: 700px;
   width: 100%;
-  background-size: cover;
-  background-position: center center;
   display: flex;
   justify-content: center;
 }
 
+.img-background {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  max-height: 800px;
+  object-fit: cover; /* Zoom in while preserving aspect ratio */
+}
 </style>
-
