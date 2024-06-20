aa4889a7a0710377ca1bb0059563d1c55ec02df1
Stefan Gouldson <stefangouldson@gmail.com>
2024-06-20 02:45:10 +0100

    update setup command to have latest features

--- storyblok/Hero.vue
+++ storyblok/Hero.vue
@@ -15,8 +15,12 @@
       />
       <div class="grid grid-cols-1 lg:grid-cols-3 z-10 w-full py-6 sm:px-5 min-h-full container py-48">
         <div class="flex flex-col justify-center px-8 sm:px-24 lg:col-span-2" :class="blok.text_position">
-          <h1 class="mb-3 z-30 font-heading" :style="{color: blok.title_color?.color}">{{ blok?.title || 'Page Title' }}</h1>
-          <p class="font-light z-30" :style="{color: blok.text_color?.color}">{{ blok?.text }}</p>
+          <h1 class="mb-3 z-30 font-heading text-gray-800 dark:text-gray-100" :style="{color: blok.title_color?.color}">
+            {{ blok?.title || 'Page Title' }}
+          </h1>
+          <p class="font-light z-30 text-gray-800 dark:text-gray-100" :style="{color: blok.text_color?.color}">
+            {{ blok?.text }}
+          </p>
           <div class="mt-10 flex flex-wrap z-30" v-if="blok?.buttons">
             <StoryblokComponent
               class="mr-3 mb-3 w-full sm:w-auto"
